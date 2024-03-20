d4b73a81ab97c56caaf5a14da10de3e1628914c7
Technicfan <lenni.ahrens@web.de>
2024-03-20 22:54:58 +0100

    optimized

--- pascal_triangle.py
+++ pascal_triangle.py
@@ -12,25 +12,23 @@ def get_triangle(toline):
     # init string variable
     triangle = ""
     for i in range(0, toline + 1):
-        # calculate space between ":" and line
-        space = " "
-        for i2 in range(0, len(str(toline))-len(str(i))):
-            space += " "
+        # calculate spaces between ":" and line
+        spaces = " " + " " * (len(str(toline))-len(str(i)))
         # get current line
-        line = str(i) + ":" + space
-        for i3 in range(0, i + 1):
-            line += str(Pas(i, i3)) + "|"
+        line = str(i) + ":" + spaces
+        for i2 in range(0, i + 1):
+            line += str(Pas(i, i2)) + "|"
         # add line to string
         triangle += line[:-1] + "\n"
     return triangle[:-1]
 
 def main(args):
-    # only proceed if first arg is an integer,
+    # only proceed if first arg is an integer
     # otherwise print usage information
     if len(args) > 1 and args[1].isdigit():
         print(get_triangle(int(args[1])))
     else:
-        print("Usage:\npas_dreieck.py <int number>\n number: to which line it should calculate")
+        print("Usage:\npas_dreieck.py <integer number>\n number: to which line it should calculate")
         exit(1)
 
 main(sys.argv)
